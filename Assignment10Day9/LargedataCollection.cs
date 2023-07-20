964dd9c7b68d9fd024487680b08ffaa07d627b58
ashishrathore98 <ashrathore98@gmail.com>
2023-07-20 16:46:32 +0530

    first commit

---
+++ Assignment10Day9/LargedataCollection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment11
+{
+
+
+    public class LargeDataCollection : IDisposable
+    {
+        private int[] data; //I am using Array as  Internal data structure (Example: array of integers)
+
+        public LargeDataCollection(int[] initialData)
+        {
+            // Here It Initialize the data structure with the initial data
+            data = new int[initialData.Length];
+            Array.Copy(initialData, data, initialData.Length);
+        }
+
+        //A  Method to add elements to the collection
+        public void AddElement(int element)
+        {
+            Array.Resize(ref data, data.Length + 1);
+            data[data.Length - 1] = element;
+        }
+
+        // Its a Method to remove elements from the collection
+        public bool RemoveElement(int element)
+        {
+            int index = Array.IndexOf(data, element);
+            if (index >= 0)
+            {
+                for (int i = index; i < data.Length - 1; i++)
+                {
+                    data[i] = data[i + 1];
+                }
+                Array.Resize(ref data, data.Length - 1);
+                return true;
+            }
+            return false;
+        }
+
+        // A Method to access elements in the collection by index
+        public int GetElement(int index)
+        {
+            if (index >= 0 && index < data.Length)
+            {
+                return data[index];
+            }
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+        }
+
+        // Implement the Dispose method to release the resources 
+        public void Dispose()
+        {
+
+
+            // Set the internal data structure to null to free up memory
+            data = null;
+        }
+    }
+}
---
+++ Assignment10Day9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment11
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the 'LargeDataCollection' class and demonstrate its usage
+            int[] initialData = { 10, 30, 50, 70, 90 };
+            using (LargeDataCollection collection = new LargeDataCollection(initialData))
+            {
+                Console.WriteLine("Initial Data in Collection:");
+                for (int i = 0; i < initialData.Length; i++)
+                {
+                    Console.WriteLine(collection.GetElement(i));
+                }
+
+                collection.AddElement(60);
+                Console.WriteLine("Added 60 to the Collection.");
+
+                collection.RemoveElement(30);
+                Console.WriteLine("Removed 30 from the Collection.");
+
+                Console.WriteLine("Data in Collection after Changes:");
+                for (int i = 0; i < initialData.Length; i++)
+                {
+                    Console.WriteLine(collection.GetElement(i));
+                }
+
+                // 'Dispose' method will be called automatically after exiting the 'using' block/ above block
+            }
+
+            // After using the 'LargeDataCollection' object, its resources will be released and memory freed
+            // The data ll be updated then .
+        }
+    }
+}
